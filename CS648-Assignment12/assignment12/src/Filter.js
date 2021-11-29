05a776c782fcc28d784e805875ec27b669588c5f
jyoti-nalwade5 <jyoti.nalwade5@gmail.com>
2021-11-28 23:34:39 -0800

    final assignment

--- CS648-Assignment12/assignment12/src/Filter.js
+++ CS648-Assignment12/assignment12/src/Filter.js
@@ -2,14 +2,21 @@ import React, { Component } from 'react'
 
 class Filter extends Component {
 
+
+    handleChange = (e) => {
+        this.props.onFilter({
+            filterText : e.target.value
+       });
+    }
+
     render() {
         return (
-            <div>
+            <div class="my-3">
                 <input 
-                        
+                        class="form-control" 
                         type="text" 
                         placeholder="Search..."
-                        >        
+                        onChange={this.handleChange}>        
                 </input>
             </div>
         )
@@ -17,3 +24,5 @@ class Filter extends Component {
 }
 
 export default Filter;
+
+
